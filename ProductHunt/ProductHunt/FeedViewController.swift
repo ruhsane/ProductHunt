733d17f85ff2e54a0c7ea7b574d6a2e9ba1d6346
Rushani Shawuti <ruhsane924@gmail.com>
2019-02-26 14:49:28 -0800

    users can browse products

--- ProductHunt/ProductHunt/FeedViewController.swift
+++ ProductHunt/ProductHunt/FeedViewController.swift
@@ -12,20 +12,27 @@ class FeedViewController: UIViewController {
 
     @IBOutlet weak var feedTableView: UITableView!
     
-    var mockData: [Post] = {
-        var meTube = Post(id: 0, name: "MeTube", tagline: "Stream videos for free!", votesCount: 25, commentsCount: 4)
-        var boogle = Post(id: 1, name: "Boogle", tagline: "Search anything!", votesCount: 1000, commentsCount: 50)
-        var meTunes = Post(id: 2, name: "meTunes", tagline: "Listen to any song!", votesCount: 25000, commentsCount: 590)
-        
-        return [meTube, boogle, meTunes]
-    }()
+    var posts: [Post] = []{
+        didSet {
+            feedTableView.reloadData()
+        }
+    }
     
+    var networkManager = NetworkManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         feedTableView.dataSource = self
         feedTableView.delegate = self
+        updateFeed()
+    }
+    
+    func updateFeed() {
+        networkManager.getPosts { (result) in
+            self.posts = result
+        }
     }
     
 }
@@ -35,14 +42,14 @@ class FeedViewController: UIViewController {
 extension FeedViewController: UITableViewDataSource {
     /// Determines how many cells will be shown on the table view.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return mockData.count
+        return posts.count
     }
     
     /// Creates and configures each cell.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath) as! PostTableViewCell
         
-        let post = mockData[indexPath.row]
+        let post = posts[indexPath.row]
         cell.post = post
         
         return cell
